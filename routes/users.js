ffd0fb5ce524ebb5635b16fcc71fa827ac33f4b7
Kevin <kevin.rph.lee@gmail.com>
2018-06-02 12:18:26 -0700

    created register user route

--- routes/users.js
+++ routes/users.js
@@ -5,9 +5,41 @@ const router  = express.Router();
 
 module.exports = (knex) => {
 
+  //Checks for valid email
+  function validateEmail(mail) {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
+      return (true)
+    }
+      return (false)
+  }
+
   router.get("/", (req, res) => {
     //Example Route
   });
+
+  router.post("/new", (req, res) => {
+    const email = req.body.email.trim().toLowerCase();
+    const password = req.body.password.trim();
+    //Converting bnet ID into a format that owjs can take
+
+    if (!(validateEmail(email))){
+      return res.status(400).send('Invalid email format!');
+    }
+    //checking to prevent email dupes
+    knex('users')
+      .select("email")
+      .from("users")
+      .where({email:email})
+      .then((results) => {
+        if (results.length === 0) {
+          console.log('Ready to insert');
+          //stuff tha relies on it
+        } else {
+          res.status(400).send("Looks like you're already enrolled! Please check your email...");
+        }
+      });
+  });
+
 
   return router;
 }
