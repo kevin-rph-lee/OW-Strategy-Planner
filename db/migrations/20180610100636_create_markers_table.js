c7410c6b94a03d3377573c4bb481a35d96aefa3f
Kevin <kevin.rph.lee@gmail.com>
2018-06-10 10:42:57 -0700

    created marker seed

--- db/migrations/20180610100636_create_markers_table.js
+++ db/migrations/20180610100636_create_markers_table.js
@@ -15,10 +15,3 @@ exports.down = function(knex, Promise) {
   return knex.schema.dropTable('markers');
 };
 
-
-
-  const locations = [
-    {pos:{lat: 37.969, lng: -122.246}, title: 'test1', type: 'parking', id: 1, desc: 'Good parking1'},
-    {pos:{lat: 37.769, lng: -122.446}, title: 'test2', type: 'parking', id: 2, desc: 'Good parking2'},
-    {pos:{lat: 37.469, lng: -122.646}, title: 'test3', type: 'parking', id: 3, desc: 'Good parking3'}
-  ];
---
+++ db/seeds/markers.js
@@ -0,0 +1,13 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('markers').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('markers').insert([
+        {position:{lat: 37.969, lng: -122.246}, title: 'test1', type: 'parking', description: 'Good parking1', owner_id: 1, map_id:1},
+        {position:{lat: 37.769, lng: -122.546}, title: 'test2', type: 'parking', description: 'Good parking2', owner_id: 1, map_id:1},
+        {position:{lat: 37.469, lng: -122.246}, title: 'test3', type: 'parking', description: 'Good parking3', owner_id: 1, map_id:1}
+      ]);
+    });
+};
