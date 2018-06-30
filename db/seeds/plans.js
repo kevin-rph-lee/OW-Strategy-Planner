e2f9d21f037f6a1e2cefec3dfa009124743eda39
Kevin <kevin.rph.lee@gmail.com>
2018-06-30 11:31:18 -0700

    added plans seed

---
+++ db/seeds/plans.js
@@ -0,0 +1,11 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('plans').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('plans').insert([
+        {name: 'Pirate Ship', description: 'RRRRRR', owner_id: 1}
+      ]);
+    });
+};
