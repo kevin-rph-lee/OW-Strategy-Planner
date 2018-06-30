319b93e258e46853a6cd88f3835e05c7809bc9a9
Kevin <kevin.rph.lee@gmail.com>
2018-06-30 12:44:59 -0700

    passing map URL to ejs file

--- routes/plans.js
+++ routes/plans.js
@@ -7,17 +7,19 @@ module.exports = (knex) => {
 
   router.get("/:id", (req, res) => {
     knex
-      .select('plans.owner_id','markers.description','markers.title','markers.position','markers.description','markers.image','markers.type','users.email', 'markers.id', 'marker_types.icon_file_location')
+      .select('plans.owner_id','markers.description','markers.title','markers.position','markers.description','markers.image','markers.type','users.email', 'markers.id', 'marker_types.icon_file_location', 'maps.url')
       .from('markers')
       .where({plan_id:req.params.id})
       .innerJoin('users', 'users.id', 'markers.owner_id')
       .innerJoin('marker_types', 'marker_types.id', 'markers.marker_type_id')
       .innerJoin('plans', 'plans.id', 'markers.plan_id')
+      .innerJoin('maps', 'plans.map_id', 'maps.id')
       .then((markers) => {
         let isOwner = false;
         if(markers[0].owner_id === req.session.userID){
           isOwner = true;
         }
+
         knex
           .select('*')
           .from('marker_types')
