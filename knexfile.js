41578e0db3b91e7ee9f7117ffae75e11708aa86a
Kevin Lee <kevin.rph.lee@gmail.com>
2018-07-19 16:21:05 -0700

    updated knexfile for heroku

--- knexfile.js
+++ knexfile.js
@@ -3,34 +3,76 @@ require('dotenv').config();
 module.exports = {
 
   development: {
-    client: 'postgresql',
-    connection: {
-      host     : process.env.DB_HOST,
-      user     : process.env.DB_USER,
-      password : process.env.DB_PASS,
-      database : process.env.DB_NAME,
-      port     : process.env.DB_PORT,
-      ssl      : process.env.DB_SSL
-    },
+    client: 'pg',
+    debug: true,
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: './db/migrations',
       tableName: 'migrations'
     },
     seeds: {
       directory: './db/seeds'
-    }
+    },
+    ssl: true
   },
 
   production: {
-    client: 'postgresql',
-    connection: process.env.DATABASE_URL + '?ssl=true',
-    pool: {
-      min: 2,
-      max: 10
-    },
+    client: 'pg',
+    debug: true,
+    connection: process.env.DATABASE_URL,
     migrations: {
+      directory: './db/migrations',
       tableName: 'migrations'
-    }
+    },
+    seeds: {
+      directory: './db/seeds'
+    },
+    ssl: true
   }
 
 };
+
+
+
+
+
+
+
+
+
+// require('dotenv').config();
+
+// module.exports = {
+
+//   development: {
+//     client: 'postgresql',
+//     connection: {
+//       host     : process.env.DB_HOST,
+//       user     : process.env.DB_USER,
+//       password : process.env.DB_PASS,
+//       database : process.env.DB_NAME,
+//       port     : process.env.DB_PORT,
+//       ssl      : process.env.DB_SSL
+//     },
+//     migrations: {
+//       directory: './db/migrations',
+//       tableName: 'migrations'
+//     },
+//     seeds: {
+//       directory: './db/seeds'
+//     }
+//   },
+
+//   production: {
+//     client: 'postgresql',
+//     connection: process.env.DATABASE_URL + '?ssl=true',
+//     pool: {
+//       min: 2,
+//       max: 10
+//     },
+//     migrations: {
+//       tableName: 'migrations'
+//     }
+//   }
+
+// };
