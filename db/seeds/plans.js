9f67bb35604ffc2f05d9eeef4f1e3881ec7e7453
Kevin Lee <kevin.rph.lee@gmail.com>
2018-07-22 21:32:53 -0700

    added filter by map

--- db/seeds/plans.js
+++ db/seeds/plans.js
@@ -6,21 +6,21 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('plans').insert([
         {name: 'Pirate Ship', description: 'RRRRRR', owner_id: 1, map_id: 1},
+        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 2},
+        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 3},
+        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 4},
         {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 5},
+        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 6},
+        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 7},
+        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 8},
+        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 9},
+        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 1},
+        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 2},
+        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 3},
+        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 4},
         {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 5},
-        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 5},
-        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 5},
-        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 5},
-        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 5},
-        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 5},
-        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 5},
-        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 5},
-        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 5},
-        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 5},
-        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 5},
-        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 5},
-        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 5},
-        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 5}
+        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 6},
+        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 7}
       ]);
     });
 };
