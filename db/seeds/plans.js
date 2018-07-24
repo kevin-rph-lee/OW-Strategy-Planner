c8330be51568950c85f04ec521779c1c5d3bd3cc
Kevin Lee <kevin.rph.lee@gmail.com>
2018-07-23 21:48:23 -0700

    showing date when a filter is selected

--- db/seeds/plans.js
+++ db/seeds/plans.js
@@ -7,21 +7,21 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('plans').insert([
         {name: 'Pirate Ship', description: 'RRRRRR', owner_id: 1, map_id: 1, created_datetime: moment().format("H:mm d/M/YY"), updated_datetime: moment().format("H:mm d/M/YY")},
-        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 2, created_datetime: moment().format("H:mm d/M/YY"), updated_datetime: moment().format("H:mm d/M/YY")},
-        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 3, created_datetime: moment().format("H:mm d/M/YY"), updated_datetime: moment().format("H:mm d/M/YY")},
-        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 4, created_datetime: moment().format("H:mm d/M/YY"), updated_datetime: moment().format("H:mm d/M/YY")},
-        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 5, created_datetime: moment().format("H:mm d/M/YY"), updated_datetime: moment().format("H:mm d/M/YY")},
-        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 6, created_datetime: moment().format("H:mm d/M/YY"), updated_datetime: moment().format("H:mm d/M/YY")},
-        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 7, created_datetime: moment().format("H:mm d/M/YY"), updated_datetime: moment().format("H:mm d/M/YY")},
-        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 8, created_datetime: moment().format("H:mm d/M/YY"), updated_datetime: moment().format("H:mm d/M/YY")},
-        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 9, created_datetime: moment().format("H:mm d/M/YY"), updated_datetime: moment().format("H:mm d/M/YY")},
-        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 1, created_datetime: moment().format("H:mm d/M/YY"), updated_datetime: moment().format("H:mm d/M/YY")},
-        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 2, created_datetime: moment().format("H:mm d/M/YY"), updated_datetime: moment().format("H:mm d/M/YY")},
-        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 3, created_datetime: moment().format("H:mm d/M/YY"), updated_datetime: moment().format("H:mm d/M/YY")},
-        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 4, created_datetime: moment().format("H:mm d/M/YY"), updated_datetime: moment().format("H:mm d/M/YY")},
-        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 5, created_datetime: moment().format("H:mm d/M/YY"), updated_datetime: moment().format("H:mm d/M/YY")},
-        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 6, created_datetime: moment().format("H:mm d/M/YY"), updated_datetime: moment().format("H:mm d/M/YY")},
-        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 7, created_datetime: moment().format("H:mm d/M/YY"), updated_datetime: moment().format("H:mm d/M/YY")}
+        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 2, created_datetime: moment().format("H:mm d/M/YYYY"), updated_datetime: moment().format("H:mm d/M/YYYY")},
+        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 3, created_datetime: moment().format("H:mm d/M/YYYY"), updated_datetime: moment().format("H:mm d/M/YYYY")},
+        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 4, created_datetime: moment().format("H:mm d/M/YYYY"), updated_datetime: moment().format("H:mm d/M/YYYY")},
+        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 5, created_datetime: moment().format("H:mm d/M/YYYY"), updated_datetime: moment().format("H:mm d/M/YYYY")},
+        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 6, created_datetime: moment().format("H:mm d/M/YYYY"), updated_datetime: moment().format("H:mm d/M/YYYY")},
+        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 7, created_datetime: moment().format("H:mm d/M/YYYY"), updated_datetime: moment().format("H:mm d/M/YYYY")},
+        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 8, created_datetime: moment().format("H:mm d/M/YYYY"), updated_datetime: moment().format("H:mm d/M/YYYY")},
+        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 9, created_datetime: moment().format("H:mm d/M/YYYY"), updated_datetime: moment().format("H:mm d/M/YYYY")},
+        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 1, created_datetime: moment().format("H:mm d/M/YYYY"), updated_datetime: moment().format("H:mm d/M/YYYY")},
+        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 2, created_datetime: moment().format("H:mm d/M/YYYY"), updated_datetime: moment().format("H:mm d/M/YYYY")},
+        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 3, created_datetime: moment().format("H:mm d/M/YYYY"), updated_datetime: moment().format("H:mm d/M/YYYY")},
+        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 4, created_datetime: moment().format("H:mm d/M/YYYY"), updated_datetime: moment().format("H:mm d/M/YYYY")},
+        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 5, created_datetime: moment().format("H:mm d/M/YYYY"), updated_datetime: moment().format("H:mm d/M/YYYY")},
+        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 6, created_datetime: moment().format("H:mm d/M/YYYY"), updated_datetime: moment().format("H:mm d/M/YYYY")},
+        {name: 'Test2', description: 'RRRRRR', owner_id: 1, map_id: 7, created_datetime: moment().format("H:mm d/M/YYYY"), updated_datetime: moment().format("H:mm d/M/YYYY")}
       ]);
     });
 };
--- routes/plans.js
+++ routes/plans.js
@@ -155,7 +155,7 @@ module.exports = (knex, moment) => {
 
 
     knex
-    .insert({name: req.body.planName, description: req.body.planDescription, owner_id: req.session.userID, map_id: Number(req.body.mapTypeID), created_datetime: moment().format("H:mm d/M/YY"), updated_datetime: moment().format("H:mm d/M/YY")})
+    .insert({name: req.body.planName, description: req.body.planDescription, owner_id: req.session.userID, map_id: Number(req.body.mapTypeID), created_datetime: moment().format("H:mm d/M/YYYY"), updated_datetime: moment().format("H:mm d/M/YYYY")})
     .into('plans')
     .returning('id')
     .then((results)=> {
