a71ea6c3d95fd940684d400701f7887ffca42db8
Kevin Lee <kevin.rph.lee@gmail.com>
2018-08-16 17:49:08 -0700

    added test plans

--- db/seeds/markers.js
+++ db/seeds/markers.js
@@ -5,12 +5,12 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('markers').insert([
-        {position:{lat: 0, lng: 0}, title: 'Parking Spot', marker_type_id: 3, description: 'test', owner_id: 1, step_id:1, image: true},
-        {position:{lat: 1, lng: 1}, title: 'Parking Spot', marker_type_id: 2, description: 'test', owner_id: 1, step_id:1, image: true},
-        {position:{lat: 2, lng: 2}, title: 'Parking Spot', marker_type_id: 2, description: 'test', owner_id: 1, step_id:2, image: true},
-        {position:{lat: 3, lng: 3}, title: 'Parking Spot', marker_type_id: 2, description: 'test', owner_id: 1, step_id:2, image: true},
-        {position:{lat: -3, lng: -4}, title: 'Parking Spot2', marker_type_id: 4, description: 'test', owner_id: 1, step_id:4, image: true},
-        {position:{lat: -3, lng: -4}, title: 'Parking Spot2', marker_type_id: 4, description: 'test', owner_id: 1, step_id:4, image: true}
+        // {position:{lat: 0, lng: 0}, title: 'Parking Spot', marker_type_id: 3, description: 'test', owner_id: 1, step_id:1, image: true},
+        // {position:{lat: 1, lng: 1}, title: 'Parking Spot', marker_type_id: 2, description: 'test', owner_id: 1, step_id:1, image: true},
+        // {position:{lat: 2, lng: 2}, title: 'Parking Spot', marker_type_id: 2, description: 'test', owner_id: 1, step_id:2, image: true},
+        // {position:{lat: 3, lng: 3}, title: 'Parking Spot', marker_type_id: 2, description: 'test', owner_id: 1, step_id:2, image: true},
+        // {position:{lat: -3, lng: -4}, title: 'Parking Spot2', marker_type_id: 4, description: 'test', owner_id: 1, step_id:4, image: true},
+        // {position:{lat: -3, lng: -4}, title: 'Parking Spot2', marker_type_id: 4, description: 'test', owner_id: 1, step_id:4, image: true}
         // {position:{lat: 37.769, lng: -122.546}, title: 'Camping Spot', marker_type_id: 3, description: 'Good parking2', owner_id: 1, plan_id:1, image: true},
         // {position:{lat: 37.469, lng: -122.246}, title: 'Dangerous Rocks', marker_type_id: 4, description: 'You will die here', owner_id: 1, plan_id:1, image: true}
       ]);
