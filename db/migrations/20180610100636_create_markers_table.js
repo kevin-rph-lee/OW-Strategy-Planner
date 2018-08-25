5cffa24f94da3c1c41811a44de936b589813b64f
Kevin Lee <kevin.rph.lee@gmail.com>
2018-08-25 11:26:20 -0700

    fixed marker type seeds

--- db/migrations/20180610100636_create_markers_table.js
+++ db/migrations/20180610100636_create_markers_table.js
@@ -1,7 +1,7 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('markers', function (table) {
-    table.increments('id').primary();
+    table.integer('id');
     table.string('title');
     table.string('description');
     table.integer('owner_id');
