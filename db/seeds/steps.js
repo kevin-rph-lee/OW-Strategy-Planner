b2a7528e2023c8151d38e62f3d89d6f3cfddb13f
Kevin Lee <kevin.rph.lee@gmail.com>
2018-08-29 16:46:42 -0700

    created description column in step table

--- db/seeds/steps.js
+++ db/seeds/steps.js
@@ -5,26 +5,26 @@ exports.seed = function(knex, Promise) {
   return knex('steps').del()
     .then(function () {
       return Promise.all([
-        knex('steps').insert({plan_id: 1}),
-        knex('steps').insert({plan_id: 2}),
-        knex('steps').insert({plan_id: 4}),
-        knex('steps').insert({plan_id: 5}),
-        knex('steps').insert({plan_id: 6}),
-        knex('steps').insert({plan_id: 7}),
-        knex('steps').insert({plan_id: 8}),
-        knex('steps').insert({plan_id: 9}),
-        knex('steps').insert({plan_id: 10}),
-        knex('steps').insert({plan_id: 11}),
-        knex('steps').insert({plan_id: 12}),
-        knex('steps').insert({plan_id: 13}),
-        knex('steps').insert({plan_id: 14}),
-        knex('steps').insert({plan_id: 15}),
-        knex('steps').insert({plan_id: 16}),
-        knex('steps').insert({plan_id: 17}),
-        knex('steps').insert({plan_id: 18}),
-        knex('steps').insert({plan_id: 18}),
-        knex('steps').insert({plan_id: 18}),
-        knex('steps').insert({plan_id: 18})
+        knex('steps').insert({plan_id: 1, description: null}),
+        knex('steps').insert({plan_id: 2, description: null}),
+        knex('steps').insert({plan_id: 4, description: null}),
+        knex('steps').insert({plan_id: 5, description: null}),
+        knex('steps').insert({plan_id: 6, description: null}),
+        knex('steps').insert({plan_id: 7, description: null}),
+        knex('steps').insert({plan_id: 8, description: null}),
+        knex('steps').insert({plan_id: 9, description: null}),
+        knex('steps').insert({plan_id: 10, description: null}),
+        knex('steps').insert({plan_id: 11, description: null}),
+        knex('steps').insert({plan_id: 12, description: null}),
+        knex('steps').insert({plan_id: 13, description: null}),
+        knex('steps').insert({plan_id: 14, description: null}),
+        knex('steps').insert({plan_id: 15, description: null}),
+        knex('steps').insert({plan_id: 16, description: null}),
+        knex('steps').insert({plan_id: 17, description: null}),
+        knex('steps').insert({plan_id: 18, description: "test1"}),
+        knex('steps').insert({plan_id: 18, description: "test2"}),
+        knex('steps').insert({plan_id: 18, description: "test3"}),
+        knex('steps').insert({plan_id: 18, description: "test4"})
       ]);
     });
 };
